b171013f87a4470d32015923c2fa65977d022e09
Prince Igwe <igwep297@gmail.com>
2024-02-21 17:39:00 +0100

    starting the recommendations microservice

---
+++ core/schema.py
@@ -0,0 +1,12 @@
+from ariadne import QueryType, load_schema_from_path, make_executable_schema, MutationType, SubscriptionType
+
+
+type_defs = load_schema_from_path('schemas')
+
+query = QueryType()
+
+
+mutation = MutationType()
+
+
+schema = make_executable_schema(type_defs, query, mutation, convert_names_case=True)
