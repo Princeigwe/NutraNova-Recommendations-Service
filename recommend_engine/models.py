be9dec4896a7e0e1728ff2dc6f791787b1ffbafd
Prince Igwe <igwep297@gmail.com>
2024-02-29 23:36:28 +0100

    graphql resolvers on hold, switching to kafka for synchronous nodes and relationships creation

--- recommend_engine/models.py
+++ recommend_engine/models.py
@@ -2,3 +2,31 @@ from django.db import models
 
 # Create your models here.
 import neomodel
+
+class Chef(neomodel.StructuredNode):
+  username = neomodel.StringProperty(required=True)
+  first_name = neomodel.StringProperty()
+  last_name = neomodel.StringProperty()
+
+  liked = neomodel.RelationshipTo('Recipe', 'LIKED')
+  un_liked = neomodel.RelationshipTo('Recipe', 'UN_LIKED')
+  published = neomodel.RelationshipTo('Recipe', 'PUBLISHED')
+
+
+class Recipe(neomodel.StructuredNode):
+  title = neomodel.StringProperty()
+  description = neomodel.StringProperty()
+  ingredients = neomodel.ArrayProperty(base_property=neomodel.JSONProperty())
+  instructions = neomodel.ArrayProperty(base_property=neomodel.StringProperty())
+  preparation_time = neomodel.StringProperty()
+  cooking_time = neomodel.StringProperty()
+  servings = neomodel.IntegerProperty()
+  nutritional_value = neomodel.JSONProperty()
+
+  is_tagged = neomodel.RelationshipTo('Tag', 'IS_TAGGED')
+
+  # images, video, thumbnail, status, created and published properties from the Recipe Service data model are not relevant data for the recommendations database
+
+
+class Tag(neomodel.StructuredNode):
+  name = neomodel.StringProperty()
---
+++ recommend_engine/resolvers.py
@@ -0,0 +1,5 @@
+from .models import Recipe,Tag, Chef
+
+
+def resolve_create_tag(*_, name):
+  pass
