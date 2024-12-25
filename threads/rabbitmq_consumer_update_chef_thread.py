62c927ad0523128a78c612a16dc9b9abb0f9df34
Prince Igwe <igwep297@gmail.com>
2024-12-25 02:44:31 +0100

    little comment here

--- threads/rabbitmq_consumer_update_chef_thread.py
+++ threads/rabbitmq_consumer_update_chef_thread.py
@@ -3,7 +3,7 @@ from utils.rabbitmq.consumers.consume_chef_data import consume
 
 
 class UpdateChefThread(Thread):
-  """this thread will be responsible for running the kafka consumer that will be listening for updated usernames to update chef data, in the background"""
+  """this thread will be responsible for running the rabbitmq consumer that will be listening for updated usernames to update chef data, in the background"""
   def __init__(self):
     Thread.__init__(self)
   
